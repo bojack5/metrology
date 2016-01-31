63debd7594fb8af4628e2688764fcd0c89b67588
Luis Borbolla <luis@4suredesign.com>
2016-01-31 17:18:55 -0600

    boolean models

---
+++ sistema/cron.py
@@ -0,0 +1,7 @@
+
+import kronos
+
+
+@kronos.register('0 7 * * 1')
+def facturas_no_pagadas():
+    pass    
--- sistema/models.py
+++ sistema/models.py
@@ -157,6 +157,7 @@ class Factura(models.Model):
     fecha = models.DateField()    
     orden_servicio = models.ForeignKey(Ordenes_de_servicio)
     descripcion    = models.CharField(max_length=255,null = True , blank = True)
+    pagada         = models.BooleanField(null = True )
     
 
     def __unicode__(self):
