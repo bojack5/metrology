66eee73661c26503e5f37becd4a976cc43efc1cc
Luis Borbolla <luis@4suredesign.com>
2016-02-05 18:35:07 -0600

    Facturas Vencidas por mes

--- sistema/cron.py
+++ sistema/cron.py
@@ -13,32 +13,62 @@ def facturas_no_pagadas():
     print 'funcionando'
     facturas_no_pagadas = Factura.objects.filter(pagada=False)
     texto = ''
-    for factura in facturas_no_pagadas:
-    	diferencia = datetime.date(datetime.now())-factura.fecha
-        if diferencia>timedelta(factura.orden_servicio.cotizacion.contacto.cliente.dias_de_credito):
-            texto += '''<li><b>Cliente :</b> %s<br>
-                        <b>contacto :</b> %s<br>
-                        <b>Telefono :</b> %s<br>
-                        <b>Ext. :</b> %s<br>
-                        <b>Email :</b> %s<br>
-                        <b>Fecha de la cotizacion :</b> %s<br>
-                        <b>Fecha de vencimiento :</b> %s<br>
-                        <b>Folio :</b> %s<br>
-                        <b>Importe :</b> $%s USD<br>
+    diferencia = datetime.date(datetime.now())-factura.fecha
+    inicio_mes = False
+    if datetime.date(datetime.now()).day == 1:
+        inicio_mes=True
+        for factura in facturas_no_pagadas:
+    	        	
+            if diferencia>timedelta(factura.orden_servicio.cotizacion.contacto.cliente.dias_de_credito):
+                texto += '''<li><b>Cliente :</b> %s<br>
+                            <b>contacto :</b> %s<br>
+                            <b>Telefono :</b> %s<br>
+                            <b>Ext. :</b> %s<br>
+                            <b>Email :</b> %s<br>
+                            <b>Fecha de la cotizacion :</b> %s<br>
+                            <b>Fecha de vencimiento :</b> %s<br>
+                            <b>Folio :</b> %s<br>
+                            <b>Importe :</b> $%s USD<br>
+                             </li>'''%(factura.orden_servicio.cotizacion.contacto.cliente.nombre,
+                         	           factura.orden_servicio.cotizacion.contacto.nombre,
+                         	           factura.orden_servicio.cotizacion.contacto.telefono,
+                         	           factura.orden_servicio.cotizacion.contacto.extension,
+                         	           factura.orden_servicio.cotizacion.contacto.email,
+                         	           factura.fecha,
+                         	           factura.fecha+timedelta(factura.orden_servicio.cotizacion.contacto.cliente.dias_de_credito),
+                         	           factura.id,
+                         	           factura.orden_servicio.cotizacion.importe)
+    else:
+    	for factura in facturas_no_pagadas:
+    		if diferencia==timedelta(0):
+    			texto += '''<li><b>Cliente :</b> %s<br>
+                            <b>contacto :</b> %s<br>
+                            <b>Telefono :</b> %s<br>
+                            <b>Ext. :</b> %s<br>
+                            <b>Email :</b> %s<br>
+                            <b>Fecha de la cotizacion :</b> %s<br>
+                            <b>Fecha de vencimiento :</b> %s<br>
+                            <b>Folio :</b> %s<br>
+                            <b>Importe :</b> $%s USD<br>
+                             </li>'''%(factura.orden_servicio.cotizacion.contacto.cliente.nombre,
+                         	           factura.orden_servicio.cotizacion.contacto.nombre,
+                         	           factura.orden_servicio.cotizacion.contacto.telefono,
+                         	           factura.orden_servicio.cotizacion.contacto.extension,
+                         	           factura.orden_servicio.cotizacion.contacto.email,
+                         	           factura.fecha,
+                         	           factura.fecha+timedelta(factura.orden_servicio.cotizacion.contacto.cliente.dias_de_credito),
+                         	           factura.id,
+                         	           factura.orden_servicio.cotizacion.importe)
 
-                         </li>'''%(factura.orden_servicio.cotizacion.contacto.cliente.nombre,
-                         	       factura.orden_servicio.cotizacion.contacto.nombre,
-                         	       factura.orden_servicio.cotizacion.contacto.telefono,
-                         	       factura.orden_servicio.cotizacion.contacto.extension,
-                         	       factura.orden_servicio.cotizacion.contacto.email,
-                         	       factura.fecha,
-                         	       factura.fecha+timedelta(factura.orden_servicio.cotizacion.contacto.cliente.dias_de_credito),
-                         	       factura.id,
-                         	       factura.orden_servicio.cotizacion.importe)
-    mandar_mail(texto)                     
 
-def mandar_mail(texto):
 
+
+        
+
+    mandar_mail(texto,inicio_mes)                     
+
+def mandar_mail(texto,inicio_mes):
+    
     me = "luis.borbolla@udem.edu"
     you = "luis@4suredesign.com"
 
@@ -50,53 +80,90 @@ def mandar_mail(texto):
 
         # Create the body of the message (a plain-text and an HTML version).
     text = "Hi!\nHow are you?\nHere is the link you wanted:\nhttp://www.python.org"
-    if 'li' in texto:
-    	msg['Subject'] = "Facturas Pendientes de pago de fecha : %s"%datetime.now()
-        html = """
-        <html>
-          <head></head>
-          <body>
-            <p>A continuacion se enlistan las facturas con credito vencido , sin pago a la fecha de envio de este correo<br>
-               
-            </p>
-            <ol>
-                %s
+    if inicio_mes:
+    	meses = ['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre']
+    	mes = datetime.date(datetime.now()).month
+    	if mes < 1:
+    	    mes = 12
 
-            </ol>
-          </body>
-        </html>
-        """%texto
-    else:
-    	msg['Subject'] = "No hay Facturas pendientes de pago"
-        html = """
-        <html>
-          <head></head>
-          <body>
-            <p>No hay facturas con adeudos a la fecha %s<br>
+        if 'li' in texto:
+    	    msg['Subject'] = "Facturas Pendientes de pago de fecha : %s"%datetime.now()
+            html = """
+            <html>
+              <head></head>
+              <body>
+                <p>A continuacion se enlistan las facturas con credito vencido durante el mes de %s, sin pago a la fecha de envio de este correo<br>
                
-            </p>
+                </p>
+                <ol>
+                    %s
+
+                </ol>
+              </body>
+            </html>
+            """%(meses[mes-1],texto)
+        else:
+    	    msg['Subject'] = "No hay Facturas pendientes de pago"
+            html = """
+            <html>
+              <head></head>
+              <body>
+                <p>No hay facturas con adeudos en el mes de  %s<br>
+               
+                </p>
             
-          </body>
-        </html>"""%datetime.now()   
-    # Record the MIME types of both parts - text/plain and text/html.
-    part1 = MIMEText(text, 'plain')
-    part2 = MIMEText(html, 'html')
+              </body>
+            </html>"""%meses[mes-1]
+    else:
+    	if 'li' in texto:
+    	    msg['Subject'] = "Facturas Pendientes de pago de fecha : %s"%datetime.now()
+            html = """
+            <html>
+              <head></head>
+              <body>
+                <p>A continuacion se enlistan las facturas con credito vencido , sin pago a la fecha de envio de este correo<br>
+               
+                </p>
+                <ol>
+                    %s
 
-    # Attach parts into message container.
-    # According to RFC 2046, the last part of a multipart message, in this case
-    # the HTML message, is best and preferred.
-    msg.attach(part1)
-    msg.attach(part2)
-    # Send the message via local SMTP server.
-    mail = smtplib.SMTP('smtp.gmail.com', 587)
+                </ol>
+              </body>
+            </html>
+            """%texto
+        else:
+    	    msg['Subject'] = "No hay Facturas pendientes de pago"
+            html = """
+            <html>
+              <head></head>
+              <body>
+                <p>No hay facturas con adeudos a la fecha %s<br>
+               
+                </p>
+            
+              </body>
+            </html>"""%datetime.now()
+    
 
-    mail.ehlo()
+        # Record the MIME types of both parts - text/plain and text/html.
+        part1 = MIMEText(text, 'plain')
+        part2 = MIMEText(html, 'html')
 
-    mail.starttls()
+        # Attach parts into message container.
+        # According to RFC 2046, the last part of a multipart message, in this case
+        # the HTML message, is best and preferred.
+        msg.attach(part1)
+        msg.attach(part2)
+        # Send the message via local SMTP server.
+        mail = smtplib.SMTP('smtp.gmail.com', 587)
 
-    mail.login('luis@4suredesign.com', 'borbollaSP123')
-    mail.sendmail(me, you, msg.as_string())
-    mail.quit()
+        mail.ehlo()
+
+        mail.starttls()
+
+        mail.login('luis@4suredesign.com', 'borbollaSP123')
+        mail.sendmail(me, you, msg.as_string())
+        mail.quit()
 
         
 
