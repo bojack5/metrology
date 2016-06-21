9c4ece8410bf44e6858e52f1ddfc09da3c97d457
Luis Borbolla <luis@4suredesign.com>
2016-06-20 23:20:05 -0500

    proxy

---
+++ proxy/tests.py
@@ -0,0 +1,8 @@
+from django.test import TestCase
+
+# Create your tests here.
+
+
+class TestStuff(TestCase):
+    def test_something(self):
+        self.assertEquals(2, 1 + 1)
---
+++ proxy/urls.py
@@ -0,0 +1,7 @@
+
+from django.conf.urls import patterns, include, url
+from .views import proxy_view
+
+urlpatterns = patterns('',
+    url(r'^(?P<url>.*)$', proxy_view, name='proxy'),
+)
